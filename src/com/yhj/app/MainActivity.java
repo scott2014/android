742ad745d9e1364fcd1f76bbe4fd6cf7b4104ea6
scott2014 <626306805@qq.com>
2014-04-06 14:27:38 +0800

    修改软件名称为Tertris

--- src/com/yhj/app/MainActivity.java
+++ src/com/yhj/app/MainActivity.java
@@ -1,15 +1,33 @@
 package com.yhj.app;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	
+	private Button play = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		this.play = (Button) this.findViewById(R.id.play);
+		
+		play.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(MainActivity.this,GameActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
