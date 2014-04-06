d19a1545bab247bab567fc4b9d34275782ddab0f
scott2014 <626306805@qq.com>
2014-04-06 14:21:57 +0800

    增加GameActivity

---
+++ src/com/yhj/app/GameActivity.java
@@ -0,0 +1,21 @@
+package com.yhj.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.game, menu);
+		return true;
+	}
+
+}
