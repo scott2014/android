40efbdcbc0050e557d5eea02bc6d1ef509cdbb90
scott smith <626306805@qq.com>
2014-04-29 17:52:55 +0800

    菜单弹窗

---
+++ programs/FreeNote/src/com/yhj/app/PMenu.java
@@ -0,0 +1,56 @@
+package com.yhj.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.PopupWindow;
+
+public class PMenu extends PopupWindow {
+
+	public PMenu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PMenu(Context context, AttributeSet attrs, int defStyleAttr,
+			int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PMenu(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PMenu(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	} 
+
+	public PMenu(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PMenu(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PMenu(View contentView, int width, int height, boolean focusable) {
+		super(contentView, width, height, focusable);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PMenu(View contentView, int width, int height) {
+		super(contentView, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PMenu(View contentView) {
+		super(contentView);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
