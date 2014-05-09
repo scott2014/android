3ec9fd0001c498386a5a4eda0e9d41274c345014
scott2014 <626306805@qq.com>
2014-05-09 08:33:50 +0800

    增加确认删除面板，但是取名不正确，不应该是Exit，需要修改为Confirm

---
+++ programs/FreeNote/src/com/yhj/app/Exit.java
@@ -0,0 +1,12 @@
+package com.yhj.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Exit extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(layoutResID)
+	}
+}
